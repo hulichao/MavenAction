10002aeb6935de127ad2f6db14b3b690d7a1d5bd
hulichao <jsu_hlc@163.com>
2018-03-30 12:00:00 +0800

    修改 AccountServiceTest

--- ch-12/account-parent/account-service/src/test/java/com/juvenxu/mvnbook/account/service/AccountServiceTest.java
+++ ch-12/account-parent/account-service/src/test/java/com/juvenxu/mvnbook/account/service/AccountServiceTest.java
@@ -10,6 +10,9 @@ import javax.mail.Message;
 
 import org.junit.After;
 import org.junit.Before;
+
+
+
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
